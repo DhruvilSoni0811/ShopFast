41b7e04346518e3605f07d391dfbe1a3adf1063d
DhruvilSoni0811 <sonidhruvil0811@gmail.com>
2025-11-17 10:54:42 +0000

    Pushed changes for b06_bronze_app_order_items_ingestion
    Added full code

--- Layers/Bronze/b06_bronze_app_order_items_ingestion.py
+++ Layers/Bronze/b06_bronze_app_order_items_ingestion.py
@@ -17,6 +17,7 @@
 from pyspark.sql import functions as F
 from pyspark.sql.types import *
 from delta.tables import DeltaTable
+from pyspark.sql.functions import *
 
 # COMMAND ----------
 
@@ -29,6 +30,15 @@ from delta.tables import DeltaTable
 # Example:
 # df = spark.read.format("...").load("...")
 
+source_table = "db_uci_data_team_dev_wkspc.mongo_shopfastmobileapp.app_order_items"
+
+nested_app_order_items_df = spark.read.format("delta").table(source_table)
+
+# COMMAND ----------
+
+nested_app_order_items_df.printSchema()
+# nested_app_order_items_df.display()
+
 # COMMAND ----------
 
 # MAGIC %md
@@ -38,6 +48,40 @@ from delta.tables import DeltaTable
 
 # TODO: Add data quality checks
 # Example: Check for nulls, duplicates, schema validation
+
+json_app_order_items_schema = StructType([
+    StructField("_id",StringType(), True),
+    StructField("order_id", StringType(), True),
+    StructField("sku", StringType(), True),
+    StructField("product_name", StringType(), True),
+    StructField("quantity", IntegerType(), True),
+    StructField("unit_price", DoubleType(), True),
+    StructField("size", StringType(), True),
+    StructField("color", StringType(), True),
+
+])
+
+app_order_items_df_parsed = nested_app_order_items_df.withColumn("parsed_data", from_json(col("data"), json_app_order_items_schema))
+
+# COMMAND ----------
+
+app_order_items_df_parsed.display()
+
+# COMMAND ----------
+
+app_order_items_df_flattened = app_order_items_df_parsed.select(
+    col("_id").alias("fivetran_id"),
+    col("_fivetran_synced"),
+    col("_fivetran_deleted"),
+    col("parsed_data._id").alias("order_items_record_id"),
+    col("parsed_data.order_id"),
+    col("parsed_data.sku"),
+    col("parsed_data.product_name"),
+    col("parsed_data.quantity"),
+    col("parsed_data.unit_price"),
+    col("parsed_data.size"),
+    col("parsed_data.color")
+)
 
 # COMMAND ----------
 
@@ -49,6 +93,19 @@ from delta.tables import DeltaTable
 # TODO: Write to Delta table
 # Example:
 # df.write #   .format("delta") #   .mode("append") #   .option("mergeSchema", "true") #   .saveAsTable("bronze_app_order_items")
+
+target_app_order_items_df = "db_uci_data_team_dev_wkspc.shopfast.app_order_items_flattened"
+app_order_items_df_flattened.write \
+    .format("delta") \
+    .mode("append") \
+    .option("mergeSchema", "true") \
+    .saveAsTable(target_app_order_items_df)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC select * from db_uci_data_team_dev_wkspc.shopfast.app_order_items_flattened
 
 # COMMAND ----------
 
