64da057ec7bb72bf5a7233681a85eded52a95b87
DhruvilSoni0811 <sonidhruvil0811@gmail.com>
2025-11-17 13:03:16 +0000

    Updated b07_bronze_app_cart_events_ingestion file with full code

--- Layers/Bronze/b07_bronze_app_cart_events_ingestion.py
+++ Layers/Bronze/b07_bronze_app_cart_events_ingestion.py
@@ -17,6 +17,7 @@
 from pyspark.sql import functions as F
 from pyspark.sql.types import *
 from delta.tables import DeltaTable
+from pyspark.sql.functions import *
 
 # COMMAND ----------
 
@@ -29,6 +30,14 @@ from delta.tables import DeltaTable
 # Example:
 # df = spark.read.format("...").load("...")
 
+source_table = "db_uci_data_team_dev_wkspc.mongo_shopfastmobileapp.app_inventory_sync"
+
+nested_app_inventory_df = spark.read.format("delta").table(source_table)
+
+# COMMAND ----------
+
+nested_app_inventory_df.display()
+
 # COMMAND ----------
 
 # MAGIC %md
@@ -38,6 +47,33 @@ from delta.tables import DeltaTable
 
 # TODO: Add data quality checks
 # Example: Check for nulls, duplicates, schema validation
+
+json_app_inventory_schema = StructType([
+    StructField("_id", StringType(), True),
+    StructField("sku", StringType(), True),
+    StructField("available_quantity", IntegerType(), True),
+    StructField("display_status", StringType(), True),
+    StructField("last_synced", TimestampType(), True),
+    StructField("source_warehouse", StringType(), True),
+    StructField("estimated_delivery", TimestampType(), True)
+])
+
+app_inventory_df_parsed = nested_app_inventory_df.withColumn("parsed_data", from_json(col("data"), json_app_inventory_schema))
+
+# COMMAND ----------
+
+app_inventory_df_flattened = app_inventory_df_parsed.select(
+  col("_id").alias("fivetran_id"),
+  col("_fivetran_synced"),
+  col("_fivetran_deleted"),
+  col("parsed_data._id").alias("order_inventory_id"),
+  col("parsed_data.sku"),
+  col("parsed_data.available_quantity"),
+  col("parsed_data.display_status"),
+  col("parsed_data.last_synced"),
+  col("parsed_data.source_warehouse"),
+  col("parsed_data.estimated_delivery")
+)
 
 # COMMAND ----------
 
@@ -49,6 +85,19 @@ from delta.tables import DeltaTable
 # TODO: Write to Delta table
 # Example:
 # df.write #   .format("delta") #   .mode("append") #   .option("mergeSchema", "true") #   .saveAsTable("bronze_app_cart_events")
+
+target_app_inventory_df = "db_uci_data_team_dev_wkspc.shopfast.app_inventory_flattened"
+app_inventory_df_flattened.write \
+  .format("delta") \
+  .mode("append") \
+  .option("mergeSchema", "true") \
+  .saveAsTable(target_app_inventory_df)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC select * from db_uci_data_team_dev_wkspc.shopfast.app_inventory_flattened
 
 # COMMAND ----------
 
