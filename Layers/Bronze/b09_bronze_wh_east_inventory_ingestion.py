609342613294a22ed48d4a4df5e7260cfcb6e6e0
DhruvilSoni0811 <sonidhruvil0811@gmail.com>
2025-11-18 14:17:09 +0000

    Edited b09 and b10 with full working code

--- Layers/Bronze/b09_bronze_wh_east_inventory_ingestion.py
+++ Layers/Bronze/b09_bronze_wh_east_inventory_ingestion.py
@@ -17,6 +17,31 @@
 from pyspark.sql import functions as F
 from pyspark.sql.types import *
 from delta.tables import DeltaTable
+from pyspark.sql.functions import *
+from dotenv import load_dotenv
+from pathlib import Path
+
+env_path = Path("/Workspace/Users/dhruvil@uciny.com/ShopFast/.env.development.local")
+load_dotenv(env_path, override=True)
+import os
+
+storage_account = "stgaccshopfastcindres"
+container_name = "shopfast-raw-data"
+source_folder = "warehouses/east"
+checkpoint_path = "checkpoints/csv_schema/east"
+schema_location = "schemas/east_wh_inventory_schema"
+target_table = "db_uci_data_team_dev_wkspc.shopfast.bronze_wh_east_inventory"
+
+source_path = f"abfss://{container_name}@{storage_account}.dfs.core.windows.net/{source_folder}/"
+checkpoint_full_path = f"abfss://{container_name}@{storage_account}.dfs.core.windows.net/{checkpoint_path}/"
+schema_full_path = f"abfss://{container_name}@{storage_account}.dfs.core.windows.net/{schema_location}"
+
+ADLS_ACCOUNT_KEY = os.getenv("ADLS_ACCOUNT_KEY")
+
+print("Loaded key:", ADLS_ACCOUNT_KEY)
+print("CWD:", os.getcwd())
+print("Env file exists:", os.path.isfile("../.env.development.local"))
+
 
 # COMMAND ----------
 
@@ -29,6 +54,52 @@ from delta.tables import DeltaTable
 # Example:
 # df = spark.read.format("...").load("...")
 
+csv_schema = StructType([
+    StructField("sku", StringType(), True),
+    StructField("product_description", StringType(), True),
+    StructField("quantity_on_hand", IntegerType(), True),
+    StructField("quantity_reserved", IntegerType(), True),
+    StructField("quantity_available", IntegerType(), True),
+    StructField("location", StringType(), True),
+    StructField("last_counter_date", DateType(), True),
+    StructField("batch_number", StringType(), True),
+    StructField("expiry_date", DateType(), True),
+    StructField("warehouse_id", StringType(), True),
+    StructField("export_timestamp", TimestampType(), True)
+])
+
+
+# COMMAND ----------
+
+east_wh_inventory_raw_df = (
+    spark.readStream
+        .format("cloudFiles")
+        .option("cloudFiles.format", "csv")
+        
+        .option("cloudFiles.schemaLocation", schema_full_path)
+        .option("cloudFiles.inferColumnTypes", "true")
+      #  .option("cloudFiles.schemaEvolutionMode", "addNewColumns")
+
+        .option("header", "true")
+        .option("delimiter", ",")
+        .option("quote", "\"")
+        .option("escape", "\"")
+        .option("multiLine", "false")
+        .option("encoding", "UTF-8")
+
+        .option("cloudFiles.maxFilesPerTrigger", 1000)
+        .option("cloudFiles.maxBytesPerTrigger", "10g")
+
+        .schema(csv_schema)
+        .load(source_path)
+)
+
+
+# COMMAND ----------
+
+display(east_wh_inventory_raw_df)
+
+
 # COMMAND ----------
 
 # MAGIC %md
@@ -38,6 +109,20 @@ from delta.tables import DeltaTable
 
 # TODO: Add data quality checks
 # Example: Check for nulls, duplicates, schema validation
+
+east_wh_inventory_transformed_df = (
+    east_wh_inventory_raw_df
+        .withColumn("export_timestamp", F.col("export_timestamp").cast("timestamp"))
+        .withColumn("sku", F.col("sku").cast("string"))
+        .withColumn("product_description", F.col("product_description").cast("string"))
+        .withColumn("quantity_on_hand", F.col("quantity_on_hand").cast("integer"))
+        .withColumn("quantity_reserved", F.col("quantity_reserved").cast("integer"))
+        .withColumn("quantity_available", F.col("quantity_available").cast("integer"))
+        .withColumn("location", F.col("location").cast("string"))
+        .withColumn("last_counter_date", F.col("last_counter_date").cast("date"))
+        .withColumn("batch_number", F.col("batch_number").cast("string"))
+        .withColumn("expiry_date", F.col("expiry_date").cast("date"))
+        .withColumn("warehouse_id", F.col("warehouse_id").cast("string")))
 
 # COMMAND ----------
 
@@ -49,6 +134,15 @@ from delta.tables import DeltaTable
 # TODO: Write to Delta table
 # Example:
 # df.write #   .format("delta") #   .mode("append") #   .option("mergeSchema", "true") #   .saveAsTable("bronze_wh_east_inventory")
+
+query = (east_wh_inventory_transformed_df.writeStream
+         .format("delta")
+         .outputMode("append")
+         .option("checkpointLocation", checkpoint_full_path)
+         .option("mergeSchema", "true")
+         
+         .trigger(availableNow=True)
+         .toTable(target_table))
 
 # COMMAND ----------
 
--- Layers/Bronze/b10_bronze_wh_west_inventory_ingestion.py
+++ Layers/Bronze/b10_bronze_wh_west_inventory_ingestion.py
@@ -14,9 +14,29 @@
 
 # COMMAND ----------
 
-from pyspark.sql import functions as F
+from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from delta.tables import DeltaTable
+from dotenv import load_dotenv
+load_dotenv("../.env.development.local")
+import os
+
+storage_account = "stgaccshopfastcindres"
+container_name = "shopfast-raw-data"
+source_folder = "warehouses/west"
+checkpoint_path = "checkpoints/csv_schema/west"
+schema_location = "schemas/west_wh_inventory_schema"
+target_table = "db_uci_data_team_dev_wkspc.shopfast.bronze_wh_west_inventory"
+
+source_path = f"abfss://{container_name}@{storage_account}.dfs.core.windows.net/{source_folder}/"
+checkpoint_full_path = f"abfss://{container_name}@{storage_account}.dfs.core.windows.net/{checkpoint_path}/"
+schema_full_path = f"abfss://{container_name}@{storage_account}.dfs.core.windows.net/{schema_location}"
+
+ADLS_ACCOUNT_KEY = os.getenv("ADLS_ACCOUNT_KEY")
+
+print("Loaded key:", ADLS_ACCOUNT_KEY)
+print("CWD:", os.getcwd())
+print("Env file exists:", os.path.isfile("../.env.development.local"))
 
 # COMMAND ----------
 
@@ -29,6 +49,49 @@ from delta.tables import DeltaTable
 # Example:
 # df = spark.read.format("...").load("...")
 
+csv_schema = StructType([
+    StructField("item_code", StringType(), True),
+    StructField("item_name", StringType(), True),
+    StructField("on_hand_qty", IntegerType(), True),
+    StructField("allocated_qty", IntegerType(), True),
+    StructField("free_qty", IntegerType(), True),
+    StructField("zone", StringType(), True),
+    StructField("last_physical_count", StringType(), True),
+    StructField("lot_number", StringType(), True),
+    StructField("warehouse_code", StringType(), True),
+    StructField("file_generated_at", StringType(), True)
+
+])
+
+
+# COMMAND ----------
+
+west_wh_inventory_raw_df = (
+    spark.readStream
+        .format("cloudFiles")
+        .option("cloudFiles.format", "csv")
+        
+        .option("cloudFiles.schemaLocation", schema_full_path)
+        .option("cloudFiles.inferColumnTypes", "true")
+
+        .option("header", "true")
+        .option("delimiter", "|")
+        .option("quote", "\"")
+        .option("escape", "\"")
+        .option("multiLine", "false")
+        .option("encoding", "UTF-8")
+
+        .option("cloudFiles.maxFilesPerTrigger", 1000)
+        .option("cloudFiles.maxBytesPerTrigger", "10g")
+
+        .schema(csv_schema)
+        .load(source_path)
+)
+
+# COMMAND ----------
+
+west_wh_inventory_raw_df.display()
+
 # COMMAND ----------
 
 # MAGIC %md
@@ -36,8 +99,26 @@ from delta.tables import DeltaTable
 
 # COMMAND ----------
 
+from pyspark.sql import functions as F
+
+# COMMAND ----------
+
 # TODO: Add data quality checks
 # Example: Check for nulls, duplicates, schema validation
+
+west_wh_inventory_transformed_df = (
+    west_wh_inventory_raw_df
+    .withColumn("item_code", F.col("item_code").cast("string"))
+    .withColumn("item_name", F.col("item_name").cast("string"))
+    .withColumn("on_hand_qty", F.col("on_hand_qty").cast("integer"))
+    .withColumn("allocated_qty", F.col("allocated_qty").cast("integer"))
+    .withColumn("free_qty", F.col("free_qty").cast("integer"))
+    .withColumn("zone", F.col("zone").cast("string"))
+    .withColumn("last_physical_count", F.col("last_physical_count").cast("string"))
+    .withColumn("lot_number", F.col("lot_number").cast("string"))
+    .withColumn("warehouse_code", F.col("warehouse_code").cast("string"))
+    .withColumn("file_generated_at", F.col("file_generated_at").cast("string"))
+)
 
 # COMMAND ----------
 
@@ -49,6 +130,15 @@ from delta.tables import DeltaTable
 # TODO: Write to Delta table
 # Example:
 # df.write #   .format("delta") #   .mode("append") #   .option("mergeSchema", "true") #   .saveAsTable("bronze_wh_west_inventory")
+
+query = (west_wh_inventory_transformed_df.writeStream
+         .format("delta")
+         .outputMode("append")
+         .option("checkpointLocation", checkpoint_full_path)
+         .option("mergeSchema", "true")
+         
+         .trigger(availableNow=True)
+         .toTable(target_table))
 
 # COMMAND ----------
 
